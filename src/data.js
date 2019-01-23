1b476baa7ce74d416708b1ebca1c6147ca4b6c78
ScarlettBurboa <45238149+ScarlettBurboa@users.noreply.github.com>
2019-01-23 09:49:25 -0300

    Revert "Primer fetch de la data, y filtro por genero"

--- src/data.js
+++ src/data.js
@@ -1,8 +1,1 @@
-window.movies = {
-  filterGenre: (data, condition) =>{
-    const filteredGenre = data.filter(element => {
-      return element.Genre.includes(condition,0)
-    });
-    return filteredGenre;
-  }
-}
+
