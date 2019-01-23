7da63d6674414211329688bef081bb975115026d
AndreaL <alafertteo@gmail.com>
2019-01-23 09:33:27 -0300

    Primer fetch de la data, y filtro por genero

--- data.js
+++ data.js
@@ -1,1 +1,8 @@
-
+window.movies = {
+  filterGenre: (data, condition) =>{
+    const filteredGenre = data.filter(element => {
+      return element.Genre.includes(condition,0)
+    });
+    return filteredGenre;
+  }
+}
