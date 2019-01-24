2aef3e44de59089b269ef4db30059ae809aadcba
ScarlettBurboa <scarlett.burboa@gmail.com>
2019-01-24 08:04:55 -0300

    eliminacion de data.js y parte de main.js para merge

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,3 @@
-/*document.getElementById('search_btn').addEventListener('click', function(){
-	let title = document.getElementById('title_text').value;
-	let request = new Request(`http://www.omdbapi.com/?s=${title}&y=&plot=short&tomatoes=true&apikey=e861c11a`);
-	let f = fetch(request).then(function(result){
-		return result.json();
-    })
-.then(function(data){
-	document.getElementById('search_result').innerHTML= '';
-	const searchEl = document.getElementById('search_result');
-        for(let i = 0; i < data.Search.length; i++){
-			const movieContainer = document.createElement('div');
-			movieContainer.className = 'search-result--item';
-			const titleEl = document.createElement('div');
-			titleEl.innerText = data.Search[i].Title;
-			const yearEl = document.createElement('div');
-			yearEl.innerText = data.Search[i].Year;
-			const typeEl = document.createElement('div');
-            typeEl.innerText = data.Search[i].Type;
-            const runtimeEl = document.createElement('div');
-			runtimeEl.innerText = data.Search[i].imdbID;
-			const posterEl = document.createElement('img');
-            posterEl.src = data.Search[i].Poster;
-			movieContainer.appendChild(posterEl);
-			movieContainer.appendChild(titleEl);
-			movieContainer.appendChild(yearEl);
-            movieContainer.appendChild(typeEl);
-            movieContainer.appendChild(runtimeEl);
-			searchEl.appendChild(movieContainer);
-		}
-    });
-});*/
 const entry = document.getElementById('play').addEventListener('click', () =>{
   const entryAplication = document.getElementById('entry-aplication');
   entryAplication.classList.add('hide');
